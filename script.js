2c45eed6a0e84f7b73137856de6b70aa765e8d33
teresa158 <wiamebaba2001@gmail.com>
2026-04-30 15:49:02 +0100

    Implement dynamic user profile card with JS and CSS

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const state = {
 
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-Btn');
-const dashboard = document.getElementById("dashboard");
+const container = document.getElementById("container");
 const userProfile = document.getElementById('userprofile');
 const profile = document.getElementById('profil');
 const fcontainer = document.getElementById('f-container');
@@ -20,4 +20,16 @@ const bookmarkCount = document.getElementById('bookmarkCount');
 //local storage : recuperer les favoris enregistrés dans le nav
 function saveFavorites(){
     localStorage.setItem("favorites", JSON.stringify(favoris.state));
+}
+
+// Afficher et remplir le profile
+function displayprofile(user){
+    profile.innerHTML =  ` 
+    <img src="${user.avatar_url}" alt="profile">
+    <h2>${user.name}</h2>
+    <p>@${user.login}</p>
+    <p>${user.bio || "No bio available"}</p>
+    <button class="add-favorites"> Add to favorites </button>
+    <a href="${user.html_url}" target="_blank">View on GitHub</a>
+    `
 }
